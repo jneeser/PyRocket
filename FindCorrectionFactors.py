5456141d93df4628fd42e15073a8a22124f5c0f4
jneeser <jones.neeser99@gmail.com>
2022-10-18 16:20:55 +0200

    adjusted correction facors

--- FindCorrectionFactors.py
+++ FindCorrectionFactors.py
@@ -5,12 +5,18 @@ import RegenCooling as rc
 import config
 
 def fun(c):
-    
+    global Niter 
+    global target_temperature
+
     sim = rc.HeatTransfer(config.cea, config.gas, config.chamber.geometry, config.material, config.coolant, config.cooling_geom, config.m_dot, config.m_dot_coolant, config.hot_gas_method, config.cooling_method, c, config.eta_combustion)
     sim.run_sim()
-    print('hi')
+    
+    print('Iteration:   ', Niter)
+    Niter += 1
 
-    return max(sim.out.T_c) - (510+273)
+    return max(sim.out.T_c) - target_temperature
 
+Niter = 1
+target_temperature =  510+273                  # target cooling channel exit temperature [K]
 res = scipy.optimize.fsolve(fun, 3)
-print(res)
+print('Correction factor:   ', res)
